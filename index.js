0d952a2f70967a8b547b8c9819067e8e929262f3
Michael Caballero <michael@Michaels-iMac.local>
2021-08-16 12:50:40 -0400

    added functionality to page so that background color changes on button click

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-alert('PAGE UP AND RUNNING');
+document.querySelector('button').addEventListener('click', () => {
+    const newColor = ['red', 'black', 'blue'][Math.floor(Math.random()*3)];
+    document.body.style.background = newColor;
+})
