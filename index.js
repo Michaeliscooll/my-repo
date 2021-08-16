2dfe62169ac31051818a62f90b0a762cd69031be
Michaeliscooll <88979966+Michaeliscooll@users.noreply.github.com>
2021-08-16 14:03:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ document.querySelector('button').addEventListener('click', () => {
     const newColor = ['red', 'black', 'blue'][Math.floor(Math.random()*3)];
     document.body.style.background = newColor;
 })
+
+
+document.querySelector('button').style.background = 'red';
